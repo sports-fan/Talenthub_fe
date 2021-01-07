b2525277fd112c04c47c38e30068c271ce65e231
sports-fan <lovesports801@gmail.com>
2021-01-07 07:55:45 -0800

    Create the Spinner component for loading indicator

---
+++ src/components/Spinner/spinner.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import PropTypes from 'prop-types'
+
+const styles = theme => ({
+  wrapper: {
+    width: '100%',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  progress: {
+    margin: theme.spacing.unit * 2,
+  },
+})
+
+function Spinner({classes}) {
+  return (
+    <div className={classes.wrapper}>
+      <CircularProgress size='5rem' className={classes.progress}/>
+    </div>
+  )
+}
+
+Spinner.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(Spinner)
