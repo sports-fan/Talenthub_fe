b04a91c0d7c4dc4f2e32f235483898aea1a63dfc
sports-fan <lovesports801@gmail.com>
2021-01-07 07:59:32 -0800

    Add user edit routing to admin router

--- src/routes/Admin/index.js
+++ src/routes/Admin/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Route, Switch} from 'react-router'
 import Dashboard from './routes/Dashboard'
+import EditUser from './routes/EditUser'
 import Users from 'components/Users'
 
 const Admin = ({ match }) => {
   return (
     <Switch>
       <Route path={`${match.path}/dashboard`} component={Dashboard} />
-      <Route path={`${match.path}/users`} component={Users} />
+      <Route exact path={`${match.path}/users`} component={Users} />
+      <Route path={`${match.path}/users/:id`} component={EditUser}/>
     </Switch>
   )
 }
