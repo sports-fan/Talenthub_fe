b1af315a903f52ef5f325a981f823a6dd0a98894
sports-fan <lovesports801@gmail.com>
2021-01-12 20:12:57 -0800

    Add padding-top to the body of widget component

--- src/components/Widget/styles.js
+++ src/components/Widget/styles.js
@@ -16,6 +16,7 @@ export default makeStyles(theme => ({
     boxShadow: theme.customShadows.widget,
   },
   widgetBody: {
+    paddingTop: theme.spacing(3),
     paddingBottom: theme.spacing(3),
     paddingRight: theme.spacing(3),
     paddingLeft: theme.spacing(3),
