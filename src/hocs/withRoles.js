4dc6a576be1a661f169813c3b89f3204ce861f41
sports-fan <lovesports801@gmail.com>
2021-01-14 17:51:43 -0800

    Tweak to the withRoles HOC

--- src/hocs/withRoles.js
+++ src/hocs/withRoles.js
@@ -9,16 +9,16 @@ const selector = createStructuredSelector({
 })
 
 export const isAdminOrRedir = (Component) => {
-  const Wrapper = ({me, ...props}) => me.role === ROLES.ADMIN ? <Component {...props}/> : <Redirect to={`${URL_PREFIXES[me.role]}/dashboard`}/>
+  const Wrapper = ({me, ...props}) => me.role === ROLES.ADMIN ? <Component {...props}/> : <Redirect to={`/${URL_PREFIXES[me.role]}/dashboard`}/>
   return connect(selector)(Wrapper)
 }
 
 export const isTeamManagerOrRedir = (Component) => {
-  const Wrapper = ({me, ...props}) => me.role === ROLES.TEAM_MANAGER ? <Component {...props}/> : <Redirect to={`${URL_PREFIXES[me.role]}/dashboard`}/>
+  const Wrapper = ({me, ...props}) => me.role === ROLES.TEAM_MANAGER ? <Component {...props}/> : <Redirect to={`/${URL_PREFIXES[me.role]}/dashboard`}/>
   return connect(selector)(Wrapper)
 }
 
 export const isDeveloperOrRedir = (Component) => {
-  const Wrapper = ({me, ...props}) => me.role === ROLES.DEVELOPER ? <Component {...props}/> : <Redirect to={`${URL_PREFIXES[me.role]}/dashboard`}/>
+  const Wrapper = ({me, ...props}) => me.role === ROLES.DEVELOPER ? <Component {...props}/> : <Redirect to={`/${URL_PREFIXES[me.role]}/dashboard`}/>
   return connect(selector)(Wrapper)
 }
