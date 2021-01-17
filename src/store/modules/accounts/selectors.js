24a9cb97e7d15aae57ede4019d41ea5ad7892766
sports-fan <lovesports801@gmail.com>
2021-01-16 16:14:38 -0800

    Restore selectors.js from trash

---
+++ src/store/modules/accounts/selectors.js
@@ -0,0 +1,12 @@
+import * as R from 'ramda'
+import { dataSelector, isRequestPending } from '../api'
+
+export const accountsSelector = R.compose(
+  R.path(['results']),
+  dataSelector('accounts')
+)
+export const accountsLoadingSelector = (state) => 
+  isRequestPending('accounts', 'GET')(state)
+export const accountDetailSelector = dataSelector('accountDetail')
+export const accountDetailLoadingSelector = (state) =>
+  isRequestPending('accountDetail', 'GET')(state)
