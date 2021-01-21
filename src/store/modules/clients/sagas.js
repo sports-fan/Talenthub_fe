d88a36c0ee46a716e6e8203cd02a42ee62545ade
sports-fan <lovesports801@gmail.com>
2021-01-21 15:18:47 -0800

    Implement update and get detail api call with sagas

--- src/store/modules/clients/sagas.js
+++ src/store/modules/clients/sagas.js
@@ -15,8 +15,22 @@ const createClient = apiCallSaga({
   path: 'api/developer/clients/'
 })
 
+const getClientDetail = apiCallSaga({
+  type: Types.GET_CLIENT_DETAIL,
+  method: 'GET',
+  path: ({payload}) => `api/developer/clients/${payload}/`,
+  selectorKey: 'clientDetail'
+})
+
+const updateClient = apiCallSaga({
+  type: Types.UPDATE_CLIENT,
+  method: 'PUT',
+  path:({payload:{id}}) => `api/developer/clients/${id}/`,
+})
 
 export default function* rootSaga() {
   yield takeLatest(Types.GET_CLIENTS, getClients)
   yield takeLatest(Types.CREATE_CLIENT, createClient)
+  yield takeLatest(Types.GET_CLIENT_DETAIL, getClientDetail)
+  yield takeLatest(Types.UPDATE_CLIENT, updateClient)
 }
--- src/store/modules/clients/selectors.js
+++ src/store/modules/clients/selectors.js
@@ -5,7 +5,8 @@ export const clientsSelector = R.compose(
   R.path(['results']),
   dataSelector('clients')
 )
-
 export const clientsLoadingSelector = (state) => 
   isRequestPending('clients', 'GET')(state)
-
+export const clientDetailSelector = dateSelector('clientDetail')
+export const clientDetailLoadingSelector = (state) =>
+  isRequestPending('clientDetail', 'GET')(state)
