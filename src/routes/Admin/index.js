ec76eb582647c2bd9aebff191909f70f3db98811
sports-fan <lovesports801@gmail.com>
2021-10-21 11:17:47 -0700

    Implement code-splitting admin section of app

--- src/routes/Admin/index.js
+++ src/routes/Admin/index.js
@@ -1,39 +1,42 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Route, Switch } from 'react-router'
 import PropTypes from 'prop-types'
 
-import Dashboard from 'routes/Shared/Dashboard/routes'
-import User from 'routes/Shared/User'
-import Team from './routes/Team'
-import Profile from 'routes/Shared/Profile'
-import Account from 'routes/Shared/Account'
-import Partner from 'routes/Shared/Partner'
-import Client from 'routes/Shared/Client'
-import Project from 'routes/Shared/Project'
-import FinancialRequests from 'routes/Shared/FinancialRequest'
-import Logging from 'routes/Shared/Logging'
-import MyLogs from 'routes/Shared/MyLogs'
-import Settings from 'routes/Shared/Settings'
-import FinancialReports from './routes/Reports'
 import { isAdminOrRedir } from 'hocs/withRoles'
+import Spinner from 'components/Spinner'
+const Dashboard = lazy(() => import('routes/Shared/Dashboard/routes'))
+const User = lazy(() => import('routes/Shared/User'))
+const Team = lazy(() => import('./routes/Team'))
+const Profile = lazy(() => import('routes/Shared/Profile'))
+const Account = lazy(() => import('routes/Shared/Account'))
+const Partner = lazy(() => import('routes/Shared/Partner'))
+const Client = lazy(() => import('routes/Shared/Client'))
+const Project = lazy(() => import('routes/Shared/Project'))
+const FinancialRequests = lazy(() => import('routes/Shared/FinancialRequest'))
+const Logging = lazy(() => import('routes/Shared/Logging'))
+const MyLogs = lazy(() => import('routes/Shared/MyLogs'))
+const Settings = lazy(() => import('routes/Shared/Settings'))
+const FinancialReports = lazy(() => import('./routes/Reports'))
 
 const Admin = ({ match: { path } }) => {
   return (
-    <Switch>
-      <Route path={`${path}/accounts`} component={Account} />
-      <Route path={`${path}/clients`} component={Client} />
-      <Route path={`${path}/dashboard`} component={Dashboard} />
-      <Route path={`${path}/financial-requests`} component={FinancialRequests} />
-      <Route path={`${path}/partners`} component={Partner} />
-      <Route path={`${path}/profiles`} component={Profile} />
-      <Route path={`${path}/projects`} component={Project} />
-      <Route path={`${path}/users`} component={User} />
-      <Route path={`${path}/teams`} component={Team} />
-      <Route path={`${path}/logging/:interval?`} component={Logging} />
-      <Route path={`${path}/my-logs`} component={MyLogs} />
-      <Route path={`${path}/settings`} component={Settings} />
-      <Route path={`${path}/financial-reports`} component={FinancialReports} />
-    </Switch>
+    <Suspense fallback={() => <Spinner />}>
+      <Switch>
+        <Route path={`${path}/accounts`} component={Account} />
+        <Route path={`${path}/clients`} component={Client} />
+        <Route path={`${path}/dashboard`} component={Dashboard} />
+        <Route path={`${path}/financial-requests`} component={FinancialRequests} />
+        <Route path={`${path}/partners`} component={Partner} />
+        <Route path={`${path}/profiles`} component={Profile} />
+        <Route path={`${path}/projects`} component={Project} />
+        <Route path={`${path}/users`} component={User} />
+        <Route path={`${path}/teams`} component={Team} />
+        <Route path={`${path}/logging/:interval?`} component={Logging} />
+        <Route path={`${path}/my-logs`} component={MyLogs} />
+        <Route path={`${path}/settings`} component={Settings} />
+        <Route path={`${path}/financial-reports`} component={FinancialReports} />
+      </Switch>
+    </Suspense>
   )
 }
 
