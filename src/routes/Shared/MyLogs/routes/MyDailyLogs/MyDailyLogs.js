6a108c092d20e11f934b3049ac22fe01ee975e9e
sports-fan <lovesports801@gmail.com>
2022-02-20 11:28:01 +0900

    Implement prev, next in dailylog page

--- src/routes/Shared/MyLogs/routes/MyDailyLogs/MyDailyLogs.js
+++ src/routes/Shared/MyLogs/routes/MyDailyLogs/MyDailyLogs.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useCallback } from 'react'
+import React, { useEffect, useCallback, useMemo } from 'react'
 import { withRouter } from 'react-router-dom'
 import { createStructuredSelector } from 'reselect'
 import { connect } from 'react-redux'
 import { Grid, Button } from '@material-ui/core'
+import { NavigateBefore, NavigateNext } from '@material-ui/icons'
 import { format } from 'date-fns'
 
 import LogCard from 'routes/Shared/MyLogs/components/LogCard'
@@ -18,6 +19,14 @@ const MyDailyLog = ({ getMyDailyLog, createMyDailyLog, updateMyDailyLog, myDaily
   const classes = useStyles()
   const queryObj = parseQueryString(location.search)
   const selectedDate = queryObj.date || format(new Date(), 'yyyy-MM-dd')
+
+  const oneDayPeriod = 86400000
+  const previousDateOfSelectedDate = useMemo(() => new Date(new Date(selectedDate).getTime() - oneDayPeriod), [
+    selectedDate
+  ])
+  const nextDateofSelectedDate = useMemo(() => new Date(new Date(selectedDate).getTime() + oneDayPeriod), [
+    selectedDate
+  ])
 
   const handleDateChange = useCallback(
     date => {
@@ -47,6 +56,24 @@ const MyDailyLog = ({ getMyDailyLog, createMyDailyLog, updateMyDailyLog, myDaily
       })
     })
   }, [history, location, getMyDailyLog])
+
+  const viewPrevDayLog = useCallback(() => {
+    history.push({
+      search: jsonToQueryString({
+        ...parseQueryString(location.search),
+        date: format(previousDateOfSelectedDate, 'yyyy-MM-dd')
+      })
+    })
+  }, [history, location, previousDateOfSelectedDate])
+
+  const viewNextDayLog = useCallback(() => {
+    history.push({
+      search: jsonToQueryString({
+        ...parseQueryString(location.search),
+        date: format(nextDateofSelectedDate, 'yyyy-MM-dd')
+      })
+    })
+  }, [history, location, nextDateofSelectedDate])
 
   const handleSaveAchievements = useCallback(
     content => {
@@ -95,21 +122,39 @@ const MyDailyLog = ({ getMyDailyLog, createMyDailyLog, updateMyDailyLog, myDaily
   )
 
   useEffect(() => {
-    const date = Date.now()
     getMyDailyLog({
-      date: format(date, 'yyyy-MM-dd')
+      date: selectedDate
     })
-  }, [getMyDailyLog])
+  }, [getMyDailyLog, selectedDate])
   return (
     <MyLogLayout
       interval="daily"
       actions={
-        <Grid item className={classes.actions}>
-          <LocalizedDatePicker margin="normal" label="Choose a date" value={selectedDate} onChange={handleDateChange} />
-          <Button margin="normal" variant="outlined" color="primary" onClick={viewTodayLog}>
-            Today
-          </Button>
-        </Grid>
+        <div className={classes.actions}>
+          <Grid item xs={3}>
+            <Button variant="outlined" color="primary" onClick={viewPrevDayLog}>
+              <NavigateBefore />
+            </Button>
+          </Grid>
+          <Grid item xs={3} className={classes.select}>
+            <LocalizedDatePicker
+              margin="normal"
+              label="Choose a date"
+              value={selectedDate}
+              onChange={handleDateChange}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <Button variant="outlined" color="primary" onClick={viewNextDayLog}>
+              <NavigateNext />
+            </Button>
+          </Grid>
+          <Grid item xs={3}>
+            <Button margin="normal" variant="outlined" color="primary" onClick={viewTodayLog}>
+              Today
+            </Button>
+          </Grid>
+        </div>
       }>
       <Grid item xs={6}>
         <LogCard title="Plan" content={myDailyLog?.plan} onSave={handleSavePlan} />
